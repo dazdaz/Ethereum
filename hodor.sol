cc4ced7adb7e1496a0ef5dbae777a75786ef05bc
dazdaz <darren@dazdaz.org>
2018-02-28 23:24:01 +0800

    Create hodor.sol

---
+++ hodor.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.0;
+
+/*
+Simple contract that returns a greeting
+*/
+contract Hodor {
+	address creator;
+	string greeting;
+
+	function Hodor(string _greeting) {
+		greeting = _greeting;
+		creator = .msg.sender;
+	}
+
+	// returns the current greeting
+	function greet() constant returns (string) {
+		return greeting;
+	}
+
+}
